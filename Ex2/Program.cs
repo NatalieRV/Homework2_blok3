ff3d1ea253d01215c9e4a270d0927677868e6010
Natalie R <tatysia@gmail.com>
2023-01-26 16:15:30 +0300

    Задача 2 вевысти третью цифру числа или сообщить, что ее нет

---
+++ Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Ввести цисло, вывести третью цифру числа или сообщить, что ее нет.
+
+int number = 805683;
+int index = 0;      // переменная-флажок
+
+if (number < 100)
+{
+    index = 1;
+};
+
+while (number > 1000)
+{
+    number = number / 10;
+};
+
+int position12 = number / 10;
+int position3 = number - position12 * 10;
+
+if (index == 0)
+{
+    Console.WriteLine(position3);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+};
+
+
+

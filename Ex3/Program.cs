3aa2686f8a32cf1cbd5f2efe08c4526c59b75c8f
Natalie R <tatysia@gmail.com>
2023-01-26 15:21:33 +0300

    Задача 3 о выходном дне недели

---
+++ Ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Цифра обозначает день недели, проверить выходной ли это.
+
+int number = 7;
+string answer = "Нет";
+if (number == 6)
+{
+    answer = "Да";
+};
+
+if (number == 7)
+{
+    answer = "Да";
+};
+
+Console.WriteLine(answer);
+

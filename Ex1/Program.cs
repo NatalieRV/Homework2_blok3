c27f2f9f18ff7216fe9928492e999dcc22092e8b
Natalie R <tatysia@gmail.com>
2023-01-26 14:44:53 +0300

    Задача 1 о второй цифре трехзначного числа

---
+++ Ex1/Program.cs
@@ -0,0 +1,6 @@
+﻿// На входе трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = 157;
+int position1 = number/100;
+int position2 = (number-position1*100)/10;
+Console.WriteLine(position2);
